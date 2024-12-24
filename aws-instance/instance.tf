991b4fdfb49090187a6514d3c4395e14aa7e00d8
Rutvij Bhesaniya <rutvij5992@gmail.com>
2024-12-24 10:47:03 +0530

    .gitignore added file

--- aws-instance/instance.tf
+++ aws-instance/instance.tf
@@ -9,6 +9,52 @@ resource "aws_instance" "terraform-demo" {
   }
 
   user_data = file("${path.module}/script.sh")
+
+
+  #---------------------------- file ---------------------------------------------------#
+  provisioner "file" {
+    source      = "/home/yudiz/Desktop/Terraform/aws-instance/dummy.txt" # Local file
+    destination = "/home/ubuntu/dumb"                                    # EC2 path
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/id_rsa") # Ensure this is the correct private key file
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "file" {
+    content     = "This is Terraform from content" # Local file
+    destination = "/home/ubuntu/dumb"              # EC2 path
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/id_rsa") # Ensure this is the correct private key file
+      host        = self.public_ip
+    }
+  }
+
+  #---------------------------- file ---------------------------------------------------#
+
+  #---------------------------- local-exec ------------jenkins---------------------------------------#
+
+  provisioner "local-exec" {
+    working_dir = "/home/ubuntu"
+    command     = "echo ${self.public_ip} > mypublic_ip.txt"
+  }
+
+  provisioner "local-exec" {
+    interpreter = [
+      "/usr/bin/python3", "-c"
+    ]
+    command = "print ('Hello World')"
+  }
+
+
 }
+
+
 
 
